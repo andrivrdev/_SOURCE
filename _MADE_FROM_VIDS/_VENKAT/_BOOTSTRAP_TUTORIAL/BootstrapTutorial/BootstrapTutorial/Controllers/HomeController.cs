1a225e2a1b10a28af6ee92463a740271d9bee77f
OTBDEV <OTBDEV@DESKTOP-34HB64I>
2018-07-06 12:28:50 +0200

    Added Embedded Bootstrap3 Docs

--- _MADE_FROM_VIDS/_VENKAT/_BOOTSTRAP_TUTORIAL/BootstrapTutorial/BootstrapTutorial/Controllers/HomeController.cs
+++ _MADE_FROM_VIDS/_VENKAT/_BOOTSTRAP_TUTORIAL/BootstrapTutorial/BootstrapTutorial/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Web;
 using System.Web.Mvc;
 
@@ -12,6 +14,21 @@ namespace BootstrapTutorial.Controllers
         {
             return View();
         }
+
+        public ActionResult EmbeddedBootstrap()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    ViewBag.Bootstrap3DocsLink = "http://" + ip.ToString() + "/Bootstrap3Docs/";
+                }
+            }
+
+            return View();
+        }
+        
         public ActionResult NoCode()
         {
             return View();
