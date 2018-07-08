821e3b46a337bf25162fc3142659187df2fe2d97
OTBDEV <OTBDEV@DESKTOP-34HB64I>
2018-07-08 14:58:52 +0200

    Clouds Working in Webforms App

---
+++ _TEST/_JARALLAX/_TUTORIAL_WEBFORMS/Try1/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Try1.Startup))]
+namespace Try1
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
