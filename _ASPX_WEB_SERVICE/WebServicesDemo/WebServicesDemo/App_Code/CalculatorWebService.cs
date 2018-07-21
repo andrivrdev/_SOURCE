3904afd168a0f017ee3d9dac9dcf48832531eb23
OTBDEV <OTBDEV@DESKTOP-34HB64I>
2018-07-21 13:47:52 +0200

    Tutorial - ASMX Web Service

---
+++ _ASPX_WEB_SERVICE/WebServicesDemo/WebServicesDemo/App_Code/CalculatorWebService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+/// <summary>
+/// Summary description for CalculatorWebService
+/// </summary>
+[WebService(Namespace = "http://localhost/calculator/api")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+// To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+// [System.Web.Script.Services.ScriptService]
+public class CalculatorWebService : System.Web.Services.WebService
+{
+
+    public CalculatorWebService()
+    {
+
+        //Uncomment the following line if using designed components 
+        //InitializeComponent(); 
+    }
+
+    [WebMethod]
+    public int Add(int xNumA, int xNumB)
+    {
+        return xNumA + xNumB;
+    }
+
+}
---
+++ _ASPX_WEB_SERVICE/WebServicesDemo/WebServicesDemo/CalculatorWebService.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="~/App_Code/CalculatorWebService.cs" Class="CalculatorWebService" %>
