a08306cf2520f6b87ba7e6d36326cd75d0927e26
andrivrdev <andrivrdev@users.noreply.github.com>
2019-02-24 11:48:16 +0200

    Android Notes App Learning Step 3 done

---
+++ _ANDROID/_XAMARIN_LEARNING/Notes_3/Notes/Notes/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Notes.Data;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace Notes
+{
+    public partial class App : Application
+    {
+        static NoteDatabase database;
+
+        public static NoteDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new NoteDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Notes.db3"));
+                }
+                return database;
+            }
+        }
+
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new NavigationPage(new NotesPage());
+        }
+    }
+}
