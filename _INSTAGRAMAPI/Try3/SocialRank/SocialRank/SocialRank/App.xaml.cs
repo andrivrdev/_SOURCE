44e698d30beb7d1a8fb8e731f01f8c29815cd6cf
andrivrdev <andrivrdev@users.noreply.github.com>
2020-12-16 16:18:53 +0200

    LoginPage Android

---
+++ _INSTAGRAMAPI/Try3/SocialRank/SocialRank/SocialRank/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Svg;
+using Xamarin.Forms.Xaml;
+
+namespace SocialRank
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            SvgImageSource.RegisterAssembly();
+
+            MainPage = new LoginPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
